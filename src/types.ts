3ca7b4d656297540cf9642da1732b7a972e1ab84
emmampakarinen <emma.m.pakarinen@gmail.com>
2025-01-16 15:31:06 +0200

    course data structure and study field options

---
+++ src/types.ts
@@ -0,0 +1,21 @@
+export interface Course {
+  id: number;
+  name: string;
+  duration: string;
+  term: "Spring" | "Fall" | "Anytime";
+  teacher: string;
+  subject: StudyFieldOption;
+}
+
+export enum StudyFieldOption {
+  chemical_eng = "Chemical Engineering",
+  computational_eng = "Computational engineering",
+  electrical_eng = "Electrical engineering",
+  energy_tech = "Energy technology",
+  environment_tech = "Environmental technology",
+  iem = "Industrial engineering and management",
+  mechanical_eng = "Mechanical engineering",
+  software_eng = "Software engineering",
+  business = "Business",
+  social_sci = "Social sciences",
+}
