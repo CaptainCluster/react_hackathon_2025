02ad8b76763722b4dcb6dd8a1e1ebffbf00f4139
CaptainCluster <sranta.ville@gmail.com>
2025-01-16 10:15:49 +0200

    Home page

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Error404 from "./pages/Error404";
+import Home from "./pages/Home";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Router>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="*" element={<Error404 />} />
         </Routes>
       </Router>
