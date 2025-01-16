642612ee69eaecb5e560fcc3d4bb87bbf6ae5746
CaptainCluster <sranta.ville@gmail.com>
2025-01-16 10:13:53 +0200

    Error 404 page

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
+import Error404 from "./pages/Error404";
 
 function App() {
 
@@ -7,10 +8,11 @@ function App() {
     <>
       <Router>
         <Routes>
+          <Route path="*" element={<Error404 />} />
         </Routes>
       </Router>
     </>
   )
 }
 
-export default App
+export default App;
