ce6a6634e0bf550f3ac4d4cfbcf9acaaae981d68
CaptainCluster <sranta.ville@gmail.com>
2025-01-17 12:08:24 +0200

    API for receiving courses

---
+++ src/src/api/course.ts
@@ -0,0 +1,15 @@
+import axios, { AxiosResponse } from "axios";
+import FailResponse from "../models/interfaces/response/FailResponse";
+import Course from "../models/interfaces/Course";
+
+export async function getCourses(): Promise<AxiosResponse<Course> | FailResponse> {
+  try {
+    const response = await axios.get("../data/filler.json");
+    return response;
+  } catch (error) {
+    console.error(error);
+    return {
+      msg: "Failed to receive course information",
+    }
+  }
+}
--- src/src/components/SearchCourse.tsx
+++ src/src/components/SearchCourse.tsx
@@ -1,10 +1,10 @@
 const SearchCourse = () => {
-  
+
   return (
     <>
       <h1>Search for a course</h1>
       <div>
-        
+           
       </div>
     </>
   );
