7a6181dbeaf803e340540d7ea958e366e97316d9
CaptainCluster <sranta.ville@gmail.com>
2025-01-22 13:21:34 +0200

    Amount of reviews shown

--- src/src/components/Search/CourseResult.tsx
+++ src/src/components/Search/CourseResult.tsx
@@ -12,12 +12,11 @@ const CourseResult = ({ index, courseEntry }: { index: number; courseEntry: Cour
           <p>Average grade</p>
           <img alt="Star icon" />
         </div>
-        <p>Review count</p>
+        <p>{courseEntry.reviews.length > 0 ? courseEntry.reviews.length : "No"} reviews</p>
       </div>
       <div className="px-3">
         <p className="font-bold">{courseEntry.name} ({courseEntry.credits} ECTS) - course_code</p>
         <div>
-          <p className="course-search-subject">{courseEntry.subject}</p>
           <p className="course-search-subject">Organizer: </p>
           <p className="course-search-subject">Language: </p>
           <p className="course-search-subject">Time: </p>
